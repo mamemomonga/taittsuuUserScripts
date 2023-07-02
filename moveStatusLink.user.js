523a58283d2668ff96a041f6fed9c0624816bfaf
mamemomonga <mamemomonga@gmail.com>
2023-07-02 11:49:05 +0900

    initial import

---
+++ moveStatusLink.user.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         タイッツー拡張_ステータスのリンクをタイツアイコンに移動
+// @namespace    https://gist.github.com/mamemomonga/7ec40576bfcd030c73804c64e80a9116
+// @version      0.1.1
+// @description  ポスト詳細のリンクをアイコンに移動し、ポスト内容のコピペをやりやすくする。
+// @author       mamemomonga
+// @match        https://taittsuu.com/home
+// @match        https://taittsuu.com/users/*
+// @icon         https://taittsuu.com/image/sozai/taitsu.png
+// @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function TaittsuuExCopipe() {}
+    TaittsuuExCopipe.prototype={
+        start: function() {
+            const t=this
+            t.runFilter()
+        },
+        log: function(m) {
+            console.log(m)
+        },
+        filter: function(e) {
+            const icon=$(e).find(".post-image").parent()
+            const link=$(e).find(".post-link-post")
+            icon.attr("href",link.attr("href"))
+            icon.attr("target","")
+            link.removeAttr("href")
+        },
+        runFilter: function() {
+            const t=this
+            return new Promise((resolve)=>{
+                let plt;
+                const hf=()=>{
+                    const po=$("#posts .post-wrap")
+                    if(po.length > 0) {
+                        clearInterval(plt)
+                        po.each((_,e)=>{
+                            t.filter(e)
+                        })
+                        resolve(posts)
+                    }
+                }
+                plt=setInterval(hf,10)
+            })
+        }
+    }
+    new TaittsuuExCopipe().start()
+})();
